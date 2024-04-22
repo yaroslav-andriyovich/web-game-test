966e594671e5cb0e935d7bb749f472e9963ae0a1
Yaroslav Andriyovich <clashhtml@gmail.com>
2024-04-22 18:07:41 +0300

    Added Server, HTML, TypeScript

---
+++ app/index.js
@@ -0,0 +1,5 @@
+const heading = document.createElement('h1')
+heading.textContent = 'Hello World! 2'
+
+const root = document.querySelector('#root')
+document.body.append(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    entry: './app/index.js'
+    entry: './app/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './html/template.html'
+        })
+    ],
+    mode: 'production'
 }
